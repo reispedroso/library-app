16f35b719bac9626c71965c42ce81db57fb2e5a0
reispedroso <eduardopedroso3d2y@gmail.com>
2025-03-14 16:07:57 -0300

    updates

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,5 @@
 class Author < ApplicationRecord
   has_many :books
+  has_many :categories, through: :books
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :rentals
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
